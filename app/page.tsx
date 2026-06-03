a7b3b809986b2544ec336c522ae5127ce157c716
Ashish <ashishmks06@gmail.com>
2026-06-03 20:05:03 +0530

    Url Form created and added to main page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import UrlForm from "@/components/UrlForm";
+
 export default function Home() {
     return (
     <main className="min-h-screen flex items-center justify-center">
-        <h1 className="text-5xl font-bold">
-        Breve
-        </h1>
+        <div className = "w-full max-w-xl p-4">
+            <h1 className = "text-5xl font-bold text-center mb-8">
+                Breve
+            </h1>
+            <UrlForm />
+        </div>
     </main>
     );
 }
---
+++ components/UrlForm.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useState } from "react";
+
+const UrlForm = () => {
+	const [url, setUrl] = useState("");
+	const [shortUrl, setShortUrl] = useState("");
+
+	async function handleSubmit() {
+		const response = await fetch("api/shorten", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ url }),
+		});
+
+		const data = await response.json();
+
+		if (data.shortCode) {
+			setShortUrl(`${window.location.origin}/${data.shortCode}`);
+		}
+	}
+
+	return (
+		<div className="space-y-4">
+			<input
+				type="url"
+				placeholder="https://example.com"
+				value={url}
+				onChange={(e) => setUrl(e.target.value)}
+				className="w-full border rounded p-3"
+			/>
+
+			<button
+				onClick={handleSubmit}
+				className="bg-black text-white px-4 py-2 rounded"
+			>
+				Shorten
+			</button>
+            {
+                shortUrl && (
+                    <div className = "p-3 border rounded">
+                        <p>Short URL:</p>
+                        <a href = {shortUrl} target="_blank" className="text-blue-600 underline">
+                            {shortUrl}
+                        </a>
+                    </div>
+                )
+            }
+		</div>
+	);
+}
+
+export default UrlForm;
