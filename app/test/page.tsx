f2dca3fb5c41b780530b0410aea5c6d3ece4517e
Ashish <ashishmks06@gmail.com>
2026-06-03 18:57:14 +0530

    Initail Setup of Breve

---
+++ app/test/page.tsx
@@ -0,0 +1,18 @@
+import { supabase } from "@/lib/supabase";
+
+export default async function TestPage() {
+	const { data, error } = await supabase.from("urls").select("*");
+
+	return (
+		<pre>
+			{JSON.stringify(
+				{
+					data,
+					error,
+				},
+				null,
+				2,
+			)}
+		</pre>
+	);
+}
